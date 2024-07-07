46b5d85c3769aa93e0a2e0f5ac54abc85f3ae8a4
albehofy <moreda272@gmail.com>
2024-07-07 14:36:21 +0300

    adding delete for table

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,43 @@
-const addingTask = document.getElementById("adding-task"); 
-const activities = document.getElementById("actives"); 
-const designs = document.getElementById("designs"); 
-const price = document.getElementById("price"); 
+const addingTask = document.getElementById("adding-task");
+const activities = document.getElementById("actives");
+const designs = document.getElementById("designs");
+const price = document.getElementById("price");
 let table = document.getElementById("results");
 let results = ``;
-coasts=''
+coasts = ''
 let totalCoast = 0;
 const designerName = document.getElementById("designerName");
 const designerNameText = document.getElementById("designerNameText");
 
 
-addingTask.addEventListener("click",()=>{
+addingTask.addEventListener("click", () => {
     AddingTasks();
-    window.setTimeout(EditingData(),0)
+    window.setTimeout(EditingData(), 0)
 });
 
-designerName.addEventListener("blur",()=>{
-    if(designerName.value) {
-        if(confirm("اضافة الاسم")){
+designerName.addEventListener("blur", () => {
+    if (designerName.value) {
+        if (confirm("اضافة الاسم")) {
             designerNameText.innerText = designerName.value;
-            designerName.style.display= 'none'
+            designerName.style.display = 'none'
         }
     }
 });
 
-document.querySelector('#print').addEventListener('click',()=>{
-    let list = document.querySelectorAll('.activety'); 
-    for(let item of list ){
-        let parent = item.parentElement; 
+document.querySelector('#print').addEventListener('click', () => {
+    let list = document.querySelectorAll('.activety');
+    for (let item of list) {
+        let parent = item.parentElement;
         let value = item.value;
         parent.innerHTML = `<p>${value}</p>`
         console.log(parent)
     }
-    window.print(); 
+    window.print();
 });
 
-function AddingTasks(){
-    if(activities.value && designs.value && price.value){
-
-        results +=`
+function AddingTasks() {
+    if (activities.value && designs.value && price.value) {
+        results += `
         <tr class="task-active">
         <td class="text-center task-info" scope="row"><input type="text" class="activety designer-tasks" value="${activities.value}"></td>
         
@@ -46,42 +45,53 @@ function AddingTasks(){
         <input type="text" class='designer-tasks ' value='${designs.value}'> </td>
 
         <td class="text-center task-price task-info"> <input type="text" class='designer-tasks' value='${price.value}'></td>
-
-        <td class="text-center total-coast">${Number(price.value)* Number(designs.value)}</td>
+        <td class="text-center total-coast">${Number(price.value) * Number(designs.value)}</td>
+        <td class="text-center  delete-task"><button class="btn btn-danger" onclick="removeTask(this)">delete</button></td>
         </tr>
         `
-    }else {
+    } else {
         alert("ادخل البيانات بطريقة صحيحة")
     }
-    window.setTimeout(()=>{
-        CalculatingCoast();   
-    },0)
+    window.setTimeout(() => {
+        CalculatingCoast();
+    }, 0)
     table.innerHTML = results;
 }
 
-function CalculatingCoast(){
+function CalculatingCoast() {
     totalCoast = 0;
-        coasts = document.querySelectorAll('.total-coast')
-        coasts.forEach(coast=>{
-            totalCoast += Number(coast.innerText)
-            allTotal.innerText = totalCoast;
-        })
+    coasts = document.querySelectorAll('.total-coast')
+    coasts.forEach(coast => {
+        totalCoast += Number(coast.innerText)
+        allTotal.innerText = totalCoast;
+    })
 }
 
 function EditingData() {
-    document.querySelectorAll('.designer-tasks').forEach(task=>{
-        task.addEventListener("blur",function(){
+    document.querySelectorAll('.designer-tasks').forEach(task => {
+        task.addEventListener("blur", function () {
             console.log(this.value)
-            this.setAttribute('value',this.value)
+            this.setAttribute('value', this.value)
             let dataCell = this.parentNode;
-            dataCell.parentNode.querySelector('.total-coast'); 
+            dataCell.parentNode.querySelector('.total-coast');
             dataCell.parentNode.querySelector('.total-coast').innerText = (dataCell.parentNode.querySelector('.task-value input').value) * (dataCell.parentNode.querySelector('.task-price input').value);
-            window.setTimeout(()=>{
-                CalculatingCoast();   
-            },0);
-            let res = document.getElementById('results');
-            results =  table.innerHTML; 
-            console.log(res);
+            window.setTimeout(() => {
+                CalculatingCoast();
+            }, 0);
+            results = table.innerHTML;
         })
     })
+}
+
+function removeTask(item) {
+    let cell = item.parentElement;
+    let row = cell.parentElement;
+    let table = document.getElementById('results');
+    row.remove();
+    results = table.innerHTML;
+    CalculatingCoast();
+    if(!totalCoast) {
+        allTotal.innerText = 0;
+        console.log(allTotal)
+    }
 }
